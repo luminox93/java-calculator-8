422117e599926dddd36f4050aac4f4287c4ca3dd
luminox93 <memoji1236@gmail.com>
2025-10-20 23:01:39 +0900

    refactor: Application과 Calculator 간결화
    - Application 메서드 체이닝으로 간소화
    - Calculator에서 불필요한 필드 제거
    - 인라인 객체 생성으로 코드 줄 수 감소

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,14 +9,11 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        StringParser parser = new StringParser();
         Calculator calculator = new Calculator();
 
-        String input = inputView.readInput();
-        List<Integer> numbers = parser.parse(input);
-        int result = calculator.sum(numbers);
-        outputView.printResult(result);
+        String input = new InputView().readInput();
+        List<Integer> numbers = new StringParser().parse(input);
+        outputView.printResult(calculator.sum(numbers));
     }
 }
--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -3,16 +3,8 @@ package calculator.domain;
 import java.util.List;
 
 public class Calculator {
-    private final NumberValidator numberValidator;
-
-    public Calculator() {
-        this.numberValidator = new NumberValidator();
-    }
-
     public int sum(List<Integer> numbers) {
-        numberValidator.validate(numbers);
-        return numbers.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
+        new NumberValidator().validate(numbers);
+        return numbers.stream().mapToInt(Integer::intValue).sum();
     }
 }
