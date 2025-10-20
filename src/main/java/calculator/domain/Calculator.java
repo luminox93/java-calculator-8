eed362db485d8c094c18d44b92d3526760cf3192
luminox93 <memoji1236@gmail.com>
2025-10-20 20:37:25 +0900

    refactor: Calculator null 리스트 예외 처리 추가
    null 리스트 입력 시 IllegalArgumentException 발생

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class Calculator {
 
     public int sum(List<Integer> numbers) {
+        if (numbers == null) {
+            throw new IllegalArgumentException("숫자 리스트가 null입니다");
+        }
         validateNegativeNumbers(numbers);
         return numbers.stream()
                 .mapToInt(Integer::intValue)
